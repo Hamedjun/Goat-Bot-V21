837fb52f023b0d3d4a7dd47bf9554d2fa238f644
Hamed junior <167455438+Hamedjun@users.noreply.github.com>
2024-04-28 23:03:26 +0000

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -23,9 +23,9 @@ module.exports = {
 			session2: "noon",
 			session3: "afternoon",
 			session4: "evening",
-			leaveType1: "left",
-			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userName} {type} the group"
+			leaveType1: "𝒂̀ 𝒒𝒖𝒊𝒕𝒕𝒆𝒓",
+			leaveType2: "𝒂̀ 𝒆́𝒕𝒆́ 𝒆𝒙𝒑𝒖𝒍𝒔𝒆́",
+			defaultLeaveMessage: "{userName} {type} 𝒅𝒆 𝒍'𝒆𝒎𝒑𝒊𝒓𝒆....☄️"
 		}
 	},
 
